b003da98243739a6113a64b21c67b6198a555ff3
Dino Babu <dinodsja@gmail.com>
2026-07-06 10:22:51 +0100

    script added

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+                        game 'common'
+                        author 'Vice-Forge.com'
+                        description 'Production Optimized Loadscreen'
+                        version '1.0.0'
+                        loadscreen 'index.html'
+                        loadscreen_cursor 'yes'
+                        files { 'index.html' }
